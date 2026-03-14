dd4f37fe9603b7ecacafab33c1cdb636d0832c5e
SxulzyFN <keelanpiggott108@gmail.com>
2026-03-14 07:28:25 +0000

    Update commandcosmeticstyles.js

--- commands/commandcosmeticstyles.js
+++ commands/commandcosmeticstyles.js
@@ -2,7 +2,7 @@
 
 const { SlashCommandBuilder } = require("discord.js");
 const { buildCosmeticStylesEmbed } = require("../utils/cosmeticStatsUI");
-const { getAllLockerSnapshots } = require("../storage");
+const { getAllLockerSnapshots } = require("../utils/storage");
 
 module.exports = {
   data: new SlashCommandBuilder()
