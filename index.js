8a392fa424c49d9fce67c207880704557da74c32
SxulzyFN <keelanpiggott108@gmail.com>
2026-03-14 07:33:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
-const { initDatabase } = require("./dbInit");
+const { initDatabase } = require("./database/dbInit");
 
 const client = new Client({
   intents: [
